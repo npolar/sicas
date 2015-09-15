b82e2964766653d9868bf1028ecc6f6864cf78b1
Remi Aleksander Solås <remi@npolar.no>
2015-09-15 12:56:43 +0200

    Code refactoring

--- src/sicas/main.d
+++ src/sicas/main.d
@@ -14,26 +14,25 @@ import std.regex		: matchFirst;
 
 int main(string[] args)
 {
-	ushort	captchaLength		= 6;
+	ushort	captchaLength		= 6;			// Minimum (default) captcha string length
 	string	fontName			= null;
-	string	path				= "/captcha";
 	ushort	port				= 0x51CA;		// 20938
-	ushort	imageHeight			= 32;
-	ushort	imageWidth			= 64;
+	ushort	imageHeight			= 32;			// Minimum (default) image width in pixels
+	ushort	imageWidth			= 64;			// Minimum (default) image width in pixels
 	ushort	timeout				= 120;			// Captcha timeout in seconds
 	
 	getopt(args,
-		"h|image-height",	"captcha image height (default: 32)",			&imageHeight,
-		"l|captcha-length",	"captcha string length (default: 6)",			&captchaLength,
-		"path",				"HTTP request path (default: /captcha)",		&path,
-		"p|port",			"sicas server port (default: 20938)",			&port,
-		"w|image-width",	"captcha image width (default: 64)",			&imageWidth,
-		"t|timeout",		"response timeout in seconds (default: 120)",	&timeout
+		"h|height",		"captcha image height (default: 32)",			&imageHeight,
+		"l|length",		"captcha string length (default: 6)",			&captchaLength,
+		"p|port",		"sicas server port (default: 20938)",			&port,
+		"w|width",		"captcha image width (default: 64)",			&imageWidth,
+		"t|timeout",	"response timeout in seconds (default: 120)",	&timeout
 	);
 	
-	Font* captchaFont;
+	Font* captchaFont;			// Pointer to sicas font in use
 	string[string] captchas;	// UUID-map of captcha strings
 	
+	// Use specified font if set, otherwise fallback to default
 	if(fontName)
 	{
 		if(!(fontName in fonts))
@@ -44,136 +43,145 @@ int main(string[] args)
 		else captchaFont = &fonts[fontName];
 	} else captchaFont = &fonts["default"];
 	
-	void requestHandler(HTTPServerRequest req, HTTPServerResponse res)
+	// Function called to generate new captcha image
+	void routeCaptcha(HTTPServerRequest req, HTTPServerResponse res)
 	{
-		if(req.path == path)
+		// Enforce GET method
+		enforceHTTP(req.method == HTTPMethod.GET, HTTPStatus.methodNotAllowed, "Expected method GET on path: /captcha");
+		
+		ushort width	= imageWidth;
+		ushort height	= imageHeight;
+		ushort length	= captchaLength;
+			
+		// Override width from query
+		if("width" in req.query)
+			width = max(to!ushort(req.query["width"]), imageWidth);
+		
+		// Override height from query
+		if("height" in req.query)
+			height = max(to!ushort(req.query["height"]), imageHeight);
+			
+		// Override length for query
+		if("length" in req.query)
+			length = max(to!ushort(req.query["length"]), captchaLength);
+		
+		// Create a new PNG image of specified dimensions
+		auto image = PNG(width, height);
+			
+		// Generate captcha image background
+		foreach(y; 0 .. height)
+			foreach(x; 0 .. width)
+				image.image ~= cast(char[]) [ floor(255f / width * x), floor(255f / height * y), 255, floor(255f / (height + width) * (y + x)) ];
+					
+		// Generate random captcha string
+		dchar[] randomString = new dchar[length];
+		fill(randomString[], randomCover(to!(dchar[])(captchaFont.glyphs), Random(unpredictableSeed)));
+		string captchaString = to!string(randomString);
+		
+		// Add captcha string to image
+		uint x, y, pixelOffset, glyphOffset;
+		uint offsetLeft = cast(uint) floor((width - (length * captchaFont.width)) / 2f);
+		uint offsetTop = cast(uint) floor((height - captchaFont.height) / 2f);
+			
+		foreach(glyph; captchaString)
 		{
-			// Get new captcha image
-			if(req.method == HTTPMethod.GET)
+			y = offsetTop + uniform(-5, 5);
+			
+			foreach(glyphLine; captchaFont.glyphRGBA(glyph))
 			{
-				ushort width	= imageWidth;
-				ushort height	= imageHeight;
-				ushort length	= captchaLength;
+				x = offsetLeft + (glyphOffset * captchaFont.width);
 				
-				// Override width from query
-				if("width" in req.query)
-					width = max(to!ushort(req.query["width"]), imageWidth);
-				
-				// Override height from query
-				if("height" in req.query)
-					height = max(to!ushort(req.query["height"]), imageHeight);
-					
-				// Override length for query
-				if("length" in req.query)
-					length = max(to!ushort(req.query["length"]), captchaLength);
-				
-				// Create a new PNG image of specified dimensions
-				auto image = PNG(width, height);
-				
-				// Generate captcha image background
-				foreach(y; 0 .. height)
-					foreach(x; 0 .. width)
-						image.image ~= cast(char[]) [ floor(255f / width * x), floor(255f / height * y), 255, floor(255f / (height + width) * (y + x)) ];
-						
-				// Generate random captcha string
-				dchar[] randomString = new dchar[length];
-				fill(randomString[], randomCover(to!(dchar[])(captchaFont.glyphs), Random(unpredictableSeed)));
-				string captchaString = to!string(randomString);
-				
-				// Add captcha string to image
-				uint x, y, pixelOffset, glyphOffset;
-				uint offsetLeft = cast(uint) floor((width - (length * captchaFont.width)) / 2f);
-				uint offsetTop = cast(uint) floor((height - captchaFont.height) / 2f);
-				
-				foreach(glyph; captchaString)
+				foreach(glyphPixel; glyphLine)
 				{
-					y = offsetTop + uniform(-5, 5);
-					
-					foreach(glyphLine; captchaFont.glyphRGBA(glyph))
+					if((cast(uint[]) glyphPixel).sum != 0)
 					{
-						x = offsetLeft + (glyphOffset * captchaFont.width);
-						
-						foreach(glyphPixel; glyphLine)
-						{
-							if((cast(uint[]) glyphPixel).sum != 0)
-							{
-								pixelOffset = (y * width * 4) + (x * 4);
-								image.image[pixelOffset .. pixelOffset + 4] = glyphPixel;
-							}
-							
-							++x;
-						}
-						++y;
+						pixelOffset = (y * width * 4) + (x * 4);
+						image.image[pixelOffset .. pixelOffset + 4] = glyphPixel;
 					}
 					
-					++glyphOffset;
+					++x;
 				}
-				
-				// Generate base64-encoded cookie key and captcha UUID
-				string cookieKey = "sicas-" ~ toHexString(md5Of(randomUUID().toString())).idup;
-				string uuid = randomUUID().toString();
-				
-				// Add captcha to cache and timeout for removal
-				captchas[uuid] = captchaString;
-				setTimer((timeout).seconds, { captchas.remove(uuid); });
-				
-				// Add cookie with UUID and expire time
-				res.setCookie(cookieKey, uuid);
-				res.cookies[cookieKey].maxAge = timeout;
-				
-				// Add captcha image to response body
-				res.writeBody(cast(ubyte[]) image.data, "image/png");
+				++y;
 			}
 			
-			// Verify captcha string
-			else if(req.method == HTTPMethod.POST)
+			++glyphOffset;
+		}
+		
+		// Generate base64-encoded cookie key and captcha UUID
+		string cookieKey = "sicas-" ~ toHexString(md5Of(randomUUID().toString())).idup;
+		string uuid = randomUUID().toString();
+			
+		// Add captcha to cache and timeout for removal
+		captchas[uuid] = captchaString;
+		setTimer((timeout).seconds, { captchas.remove(uuid); });
+		
+		// Add cookie with UUID and expire time
+		res.setCookie(cookieKey, uuid);
+		res.cookies[cookieKey].maxAge = timeout;
+		
+		// Add captcha image to response body
+		res.writeBody(cast(ubyte[]) image.data, "image/png");
+	}
+	
+	// Function called to validate captcha string
+	void routeValidate(HTTPServerRequest req, HTTPServerResponse res)
+	{
+		// Enforce POST method
+		enforceHTTP(req.method == HTTPMethod.POST, HTTPStatus.methodNotAllowed, "Expected method POST on path: /validate");
+		
+		if("sicas" in req.form)
+		{
+			string captcha = req.form["sicas"];
+			string uuid;
+			
+			foreach(cookie; req.cookies)
 			{
-				if("sicas" in req.form)
+				auto match = matchFirst(cookie.name, "^sicas-[A-F0-9]{32}$");
+				
+				if(!match.empty && ((uuid = cookie.value) in captchas))
 				{
-					string captcha = req.form["sicas"];
-					string uuid;
-					
-					foreach(cookie; req.cookies)
+					if(captchas[uuid] == captcha)
 					{
-						auto match = matchFirst(cookie.name, "^sicas-[A-F0-9]{32}$");
-						
-						if(!match.empty && ((uuid = cookie.value) in captchas))
-						{
-							if(captchas[uuid] == captcha)
-							{
-								res.writeBody("Success!", "text/plain");
-								res.statusCode = 200;
-								return;
-							}
-						}
+						res.writeBody("Success!", "text/plain");
+						res.statusCode = HTTPStatus.ok;
+						return;
 					}
-					
-					if(uuid)
-					{
-						// Return 403 (Forbidden) if the captcha validation failed
-						res.writeBody("Invalid captcha", "text/plain");
-						res.statusCode = 403;
-					}
-					else
-					{
-						// Return 410 (Gone) if the captcha doesn't exist (has timed-out)
-						res.writeBody("Captcha timeout", "text/plain");
-						res.statusCode = 410;
-					}
-				}
-				else
-				{
-					// Return 400 (Bad Request) if sicas was not found as a form POST element
-					res.writeBody("Missing captcha", "text/plain");
-					res.statusCode = 400;
 				}
 			}
+			
+			if(uuid)
+			{
+				// Return 403 (Forbidden) if the captcha validation failed
+				res.writeBody("Invalid captcha", "text/plain");
+				res.statusCode = HTTPStatus.forbidden;
+			}
+			else
+			{
+				// Return 410 (Gone) if the captcha doesn't exist (has timed-out)
+				res.writeBody("Captcha timeout", "text/plain");
+				res.statusCode = HTTPStatus.gone;
+			}
+		}
+		else
+		{
+			// Return 400 (Bad Request) if sicas was not found as a form POST element
+			res.writeBody("Missing captcha", "text/plain");
+			res.statusCode = HTTPStatus.badRequest;
 		}
 	}
 	
+	// Create URL routes for captcha generation/validation
+	auto router = new URLRouter;
+	router
+	.get("/captcha", &routeCaptcha)
+	.post("/validate", &routeValidate);
+	
+	// Set specified lister port, and enable worker-thread distribution
 	auto httpSettings = new HTTPServerSettings;
+	httpSettings.options |= HTTPServerOption.distribute;
 	httpSettings.port = port;
 	
-	listenHTTP(httpSettings, &requestHandler);
+	// Start HTTP listening, and run event loop
+	listenHTTP(httpSettings, router);
 	return runEventLoop();
 }
